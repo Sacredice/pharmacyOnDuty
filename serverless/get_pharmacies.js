384f8992372ff1e6048c2103b67c6512cd188fe1
Sacredice <122034500+Sacredice@users.noreply.github.com>
2024-03-02 02:25:00 +0300

    fetch serverless

--- serverless/get_pharmacies.js
+++ serverless/get_pharmacies.js
@@ -1,20 +1,21 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 
-const {REACT_APP_COLLECTAPI_KEY} = process.env;
+const { REACT_APP_COLLECTAPI_KEY } = process.env;
 
 exports.handler = async (event, context) => {
     const param = JSON.parse(event.body);
     const { city } = params;
     try {
-        const response = await axios(`https://api.collectapi.com/health/dutyPharmacy?il=${city}`, {
+        const response = await fetch(`https://api.collectapi.com/health/dutyPharmacy?il=${city}`, {
             headers: {
               "authorization": process.env.REACT_APP_COLLECTAPI_KEY,
               "content-type": "application/json"
             }
-    });
+        });
+        const responseJson = await response.json();
         return {
             statusCode: 200,
-            body: JSON.stringify(response.data)
+            body: JSON.stringify(response)
         };
     } catch (err) {
         return { statusCode: 422, body: err.stack };
